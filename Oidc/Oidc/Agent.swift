b4e8d4c4967518de604673488c7171e2e028ad5c
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2024-12-09 16:43:48 -0500

    Add missing docs to PingOidc

--- Oidc/Oidc/Agent.swift
+++ Oidc/Oidc/Agent.swift
@@ -1,6 +1,6 @@
 //
 //  Agent.swift
-//  Oidc
+//  PingOidc
 //
 //  Copyright (c) 2024 Ping Identity. All rights reserved.
 //
@@ -16,6 +16,8 @@ import Foundation
 public protocol Agent<T> {
      associatedtype T
      
+     /// Provides the configuration object for the `Agent`.
+     /// - Returns: A function that returns the configuration object.
      func config() -> () -> T
      
      /// End the session with the OpenID Connect provider.
@@ -36,53 +38,83 @@ public class OidcConfig<T> {
      let oidcClientConfig: OidcClientConfig
      let config: T
      
-     public init(oidcClientConfig: OidcClientConfig, config: T) {
+     /// Initialize the `OidcConfig` with the `OidcClientConfig` and the configuration object.
+     /// - Parameters:
+     ///   - oidcClientConfig: The client configuration for the OpenID Connect provider.
+     ///   - config: The configuration object.
+     init(oidcClientConfig: OidcClientConfig, config: T) {
           self.oidcClientConfig = oidcClientConfig
           self.config = config
      }
 }
 
+
+/// Default implementation of the  `Agent` interface.
 public class DefaultAgent: Agent {
      
      public typealias T = Void
      
+     /// Initialize the `DefaultAgent`.
      public init() {}
      
+     /// Provides an empty configuration for the `DefaultAgent`.
+     /// - Returns: A function that returns Void
      public func config() -> () -> Void {
           return {}
      }
      
+     /// End the session with the OpenID Connect provider. This implementation always returns false.
+     /// - Parameters:
+     ///   - oidcConfig: The configuration for the OpenID Connect client.
+     ///   - idToken: The ID token used to end the session.
+     /// - Returns: Always returns false.
      public func endSession(oidcConfig: OidcConfig<Void>, idToken: String) async -> Bool {
           return false
      }
      
+     /// Authorize the `DefaultAgent` with the OpenID Connect provider.
+     /// This implementation always throws an `OidcError.authorizeError` error.
+     /// - Parameter oidcConfig: The configuration for the OpenID Connect client.
+     /// - Returns: Never returns normally.
      public func authorize(oidcConfig: OidcConfig<Void>) async throws -> AuthCode {
           throw OidcError.authorizeError(message: "No AuthCode is available.")
      }
 }
 
+
+/// Delegate protocol to dispatch `Agent` functions
 public protocol AgentDelegateProtocol {
      associatedtype T
      func authenticate() async throws -> AuthCode
      func endSession(idToken: String) async throws -> Bool
 }
 
+
 /// Delegate class to dispatch `Agent` functions
 public class AgentDelegate<T: Any>: AgentDelegateProtocol  {
      let agent: any Agent<T>
      let oidcConfig: OidcConfig<T>
      
+     /// Initialize the `AgentDelegate` with an `Agent` and the configuration object.
+     /// - Parameters:
+     ///   - agent: The `Agent` instance.
+     ///   - agentConfig: The configuration object for the `Agent`.
+     ///   - oidcClientConfig: The `OidcClientConfig` instance .
      init(agent: any Agent<T>, agentConfig: T, oidcClientConfig: OidcClientConfig) {
           self.agent = agent
           self.oidcConfig = OidcConfig(oidcClientConfig: oidcClientConfig, config: agentConfig)
      }
      
+     /// Authenticate with the OpenID Connect provider.
+     /// - Returns: The authorization code.
      public func authenticate() async throws -> AuthCode {
           return try await self.agent.authorize(oidcConfig: oidcConfig)
      }
      
+     ///  End the session with the OpenID Connect provider.
+     /// - Parameter idToken: The ID token used to end the session.
+     /// - Returns: A boolean indicating whether the session was successfully ended.
      public func endSession(idToken: String) async throws -> Bool {
           return try await agent.endSession(oidcConfig: oidcConfig, idToken: idToken)
      }
-     
 }
