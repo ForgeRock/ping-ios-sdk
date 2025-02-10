fffbf09af5598a7b87bc51a04245d8f8df8554ea
George Bafaloukas <george.bafaloukas@forgerock.com>
2025-02-10 15:34:08 +0000

    Adding external_idp and browser tests

---
+++ Browser/PingBrowserTests/PingBrowserTests.swift
@@ -0,0 +1,19 @@
+//
+//  PingBrowserTests.swift
+//  PingBrowserTests
+//
+//  Created by george bafaloukas on 10/02/2025.
+//
+
+import XCTest
+@testable import PingBrowser
+
+final class PingBrowserTests: XCTestCase {
+
+    func testCurrentBrowser() throws {
+        let browser = BrowserLauncher.currentBrowser
+        XCTAssertNotNil(browser)
+        XCTAssertFalse(browser!.isInProgress)
+    }
+
+}
