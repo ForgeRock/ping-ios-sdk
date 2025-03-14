59dcb0758f896df6775a56ef71d36193eaa70cec
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2025-03-14 11:46:51 -0400

    Update Request class and  OrchestrateTests to Swift 6

--- Orchestrate/OrchestrateTests/SessionTests.swift
+++ Orchestrate/OrchestrateTests/SessionTests.swift
@@ -2,7 +2,7 @@
 //  SessionTests.swift
 //  OrchestrateTests
 //
-//  Copyright (c) 2024 Ping Identity. All rights reserved.
+//  Copyright (c) 2024 - 2025 Ping Identity. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
@@ -24,7 +24,7 @@ final class SessionTests: XCTestCase {
     XCTAssertEqual("session_value", session.value)
   }
   
-  class MockSession: Session {
+  struct MockSession: Session {
     var value: String = "session_value"
   }
   
