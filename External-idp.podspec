25234ff818a2329fe395a20965e7ec87f6ce6345
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2025-03-20 13:08:00 -0400

    Update podspecs and Package.swift

---
+++ External-idp.podspec
@@ -0,0 +1,39 @@
+#
+# Be sure to run `pod lib lint External-idp.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = 'External-idp'
+  s.version          = '1.0.0'
+  s.summary          = 'External-idp module for the Ping iOS SDK'
+  s.description      = <<-DESC
+  The External-idp module for the Ping iOS SDK is a library for Authentication with external IDPs when using the Ping iOS SDK.
+                       DESC
+  s.homepage         = 'https://www.pingidentity.com/'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = 'Ping Identity'
+
+  s.source           = {
+      :git => 'https://github.com/ForgeRock/ping-ios-sdk.git',
+      :tag => s.version.to_s
+  }
+
+  s.module_name   = 'External_idp'
+  s.swift_versions = ['5.0', '5.1']
+
+  s.ios.deployment_target = '13.0'
+
+  base_dir = "External-idp/External-idp"
+  s.source_files = base_dir + '/**/*.swift', base_dir + '/**/*.c', base_dir + '/**/*.h'
+  s.resource_bundles = {
+    'External-idp' => [base_dir + '/*.xcprivacy']
+  }
+
+  s.ios.dependency 'PingOrchestrate', '~> 1.0.0'
+  s.ios.dependency 'PingBrowser', '~> 1.0.0'
+    
+end
--- Package.swift
+++ Package.swift
@@ -11,7 +11,9 @@ let package = Package (
         .library(name: "PingStorage", targets: ["PingStorage"]),
         .library(name: "PingOrchestrate", targets: ["PingOrchestrate"]),
         .library(name: "PingOidc", targets: ["PingOidc"]),
-        .library(name: "PingDavinci", targets: ["PingDavinci"])
+        .library(name: "PingDavinci", targets: ["PingDavinci"]),
+        .library(name: "PingBrowser", targets: ["PingBrowser"]),
+        .library(name: "External-idp", targets: ["External-idp"])
     ],
     dependencies: [
     ],
@@ -21,5 +23,7 @@ let package = Package (
         .target(name: "PingOrchestrate", dependencies: [.target(name: "PingLogger"), .target(name: "PingStorage")], path: "Orchestrate/Orchestrate", exclude: ["Orchestrate.h"], resources: [.copy("PrivacyInfo.xcprivacy")]),
         .target(name: "PingOidc", dependencies: [.target(name: "PingOrchestrate")], path: "Oidc/Oidc", exclude: ["Oidc.h"], resources: [.copy("PrivacyInfo.xcprivacy")]),
         .target(name: "PingDavinci", dependencies: [.target(name: "PingOidc"),], path: "Davinci/Davinci", exclude: ["Davinci.h"], resources: [.copy("PrivacyInfo.xcprivacy")]),
+        .target(name: "PingBrowser", dependencies: [.target(name: "PingBrowser"),], path: "Browser/Browser", exclude: ["Browser.h"], resources: [.copy("PrivacyInfo.xcprivacy")]),
+        .target(name: "External-idp", dependencies: [.target(name: "External-idp"),], path: "External-idp/External-idp", exclude: ["ExtrernalIdp.h"], resources: [.copy("PrivacyInfo.xcprivacy")])
     ]
 )
