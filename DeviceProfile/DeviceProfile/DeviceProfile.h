70c118c3e3188992c467ef915c38c3aa2defa62a
George Bafaloukas <george.bafaloukas@forgerock.com>
2026-01-22 14:24:59 +0000

    Fixing various warnings in the project

--- DeviceProfile/DeviceProfile/DeviceProfile.h
+++ DeviceProfile/DeviceProfile/DeviceProfile.h
@@ -1,8 +1,8 @@
 //
 //  DeviceProfile.h
-//  DeviceProfile
+//  PingDeviceProfile
 //
-//  Copyright (c) 2025 Ping Identity Corporation. All rights reserved.
+//  Copyright (c) 2025 - 2026 Ping Identity Corporation. All rights reserved.
 //
 //  This software may be modified and distributed under the terms
 //  of the MIT license. See the LICENSE file for details.
@@ -11,10 +11,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for DeviceProfile.
-FOUNDATION_EXPORT double DeviceProfileVersionNumber;
+FOUNDATION_EXPORT double PingDeviceProfileVersionNumber;
 
 //! Project version string for DeviceProfile.
-FOUNDATION_EXPORT const unsigned char DeviceProfileVersionString[];
+FOUNDATION_EXPORT const unsigned char PingDeviceProfileVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <DeviceProfile/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <PingDeviceProfile/PublicHeader.h>
 
