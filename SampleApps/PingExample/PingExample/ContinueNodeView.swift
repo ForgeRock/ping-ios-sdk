cbcb2f981d348048aed8e03fd9a8d34601ff5e8d
Vahan Harutyunyan <vahan.harutyunyan@forgerock.com>
2026-04-24 10:14:39 -0400

    Add AgreementCollector support for Davinci forms

--- SampleApps/PingExample/PingExample/ContinueNodeView.swift
+++ SampleApps/PingExample/PingExample/ContinueNodeView.swift
@@ -86,6 +86,8 @@ struct ContinueNodeView: View {
                     FidoRegistrationCollectorView(collector: fidoRegistrationCollector, onNext: { onNext(true) })
                 case let fidoAuthenticationCollector as FidoAuthenticationCollector:
                     FidoAuthenticationCollectorView(collector: fidoAuthenticationCollector, onNext: { onNext(true) })
+                case let agreementCollector as AgreementCollector:
+                    AgreementView(field: agreementCollector)
                 default:
                     EmptyView()
                 }
